16b93c9102d433a8341b4995eae0c46808ce6183
Giang Le <gianglevan94@gmail.com>
2021-11-18 22:57:34 +0700

    Add some unit test

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,40 +1,53 @@
 pragma solidity >=0.7.0 <0.9.0;
 
+contract CampaignFactory {
+    Campaign[] public deployedCampaigns;
+
+    function createCampaign(uint256 minium) public {
+        Campaign campaign = new Campaign(minium, msg.sender);
+
+        deployedCampaigns.push(campaign);
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address payable recipient;
         bool complete;
-        uint approveVoteCount;
+        uint256 approveVoteCount;
         mapping(address => bool) approveVotes;
     }
-    
+
     address public manager;
-    uint public miniumContribution;
+    uint256 public miniumContribution;
     mapping(address => bool) public approvers;
-    uint public numRequest;
-    mapping (uint => Request) requests;
-    
-    constructor(uint minum) {
-        manager = msg.sender;
+    uint256 public numRequest;
+    mapping(uint256 => Request) public requests;
+
+    constructor(uint256 minum, address sender) {
+        manager = sender;
         miniumContribution = minum;
     }
-    
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
+
     function contribute() public payable {
         require(msg.value > miniumContribution);
-        
+
         approvers[msg.sender] = true;
     }
-    
-    function createRequest (uint value, address payable recipient, string memory description) public restricted {
-        uint requestIndex = numRequest++;
+
+    function createRequest(
+        uint256 value,
+        address payable recipient,
+        string memory description
+    ) public restricted {
+        uint256 requestIndex = numRequest++;
         Request storage request = requests[requestIndex];
         request.description = description;
         request.value = value;
@@ -42,19 +55,23 @@ contract Campaign {
         request.complete = false;
         request.approveVoteCount = 0;
     }
-    
-    function approve (uint index) public {
+
+    function approve(uint256 index) public {
         Request storage request = requests[index];
         require(approvers[msg.sender]);
         require(!request.approveVotes[msg.sender]);
-        
+
         request.approveVotes[msg.sender] = true;
         request.approveVoteCount++;
     }
-    
-    function getRequest (uint index) internal view returns (Request storage r) {
+
+    function getRequest(uint256 index)
+        internal
+        view
+        returns (Request storage r)
+    {
         Request storage request = requests[index];
-        
+
         return request;
     }
 }
--- test/campaign.test.js
+++ test/campaign.test.js
@@ -1,15 +1,79 @@
-const { expect } = require("chai");
+const { assert } = require("chai");
 
+const CampaignFactory = artifacts.require("CampaignFactory");
 const Campaign = artifacts.require("Campaign");
-let campaign;
 
-describe("Campaign", function () {
+describe("Campaign contract", function () {
+  let factory;
+  let accounts;
+  let campaign;
   beforeEach(async () => {
-    campaign = await Campaign.new(10000);
-  })
+    factory = await CampaignFactory.new(10000);
+    accounts = await web3.eth.getAccounts();
+    await factory.createCampaign(10000);
+    const campaignAddress = await factory.deployedCampaigns(0);
+    campaign = await new web3.eth.Contract(Campaign.abi, campaignAddress);
+  });
 
+  it("Should deploy contract success", async () => {
+    assert.ok(factory.address);
+  });
 
-  it("Should deploy contract success", async function () {
-    assert.ok(campaign.address);
+  it("The first account is the manager of created campaign", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(manager, accounts[0]);
+  });
+
+  it("Can contribute", async () => {
+    await campaign.methods.contribute().send({
+      value: 40000,
+      from: accounts[1],
+    });
+
+    assert.ok(await campaign.methods.approvers(accounts[1]).call());
+  });
+
+  it("Require minium contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: 300,
+        from: accounts[1],
+      });
+      assert.ok(false);
+    } catch (e) {
+      assert.ok(e);
+    }
+  });
+
+  it("Only manager can create request", async () => {
+    try {
+      await campaign.methods
+        .createRequest(10000, accounts[1], "Buy something")
+        .send({
+          from: accounts[1],
+        });
+      assert.ok(false);
+    } catch (e) {
+      assert.ok(e);
+    }
+  });
+
+  it("Approve method work fine", async () => {
+    await campaign.methods.contribute().send({
+      value: 40000,
+      from: accounts[1],
+    });
+    await campaign.methods
+      .createRequest(10000, accounts[1], "Buy something")
+      .send({
+        from: accounts[0],
+      });
+    // Call approve
+    await campaign.methods.approve(0).send({
+      from: accounts[1],
+    });
+    //Check the request
+    const request = await campaign.methods.requests(0).call();
+    assert.equal(request.approveVoteCount, 1);
   });
 });
